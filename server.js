5e5c7f792f02fc0a282366cfcc699a1707636378
anjanawijesooriya <anjana.wijesooriya2000@gmail.com>
2022-05-12 10:53:13 +0530

    server configuration

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB was Connected Successfully");
+});
+
+const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running in port ${PORT}`);
+});
